c06b979669a6f43f260ef0d753977b06f68cb100
Máximo Sánchez-Aragón <maxsanara@gmail.com>
2024-02-15 18:01:35 +0100

    Maxe's implements on 15th Feb.

--- examples/change_codon_example.R
+++ examples/change_codon_example.R
@@ -13,14 +13,16 @@ change.codon(3,"AAATTTNBNGGG",gcode=NULL, degenerate = TRUE)
 # change.codon(3,sq,gc=NULL, degenerate = FALSE)
 
 # EXAMPLE 4
+# Includes a list of polar and apolar residues in the Genetic Code
+# and substitutes the third codon by another codon from the same polarity group.
 # loads the codon table from the data folder
 package.name <- "silentmut"
 indexes <- which(dir.exists(paste(.libPaths(),"/",package.name,sep="")))
 load(paste(.libPaths()[indexes[1]],"/",package.name,"/data/DROME_gencode",sep=""))
 
+# Includes a polarity list of residues to the genetic code
 polar.aa <- c("R","S","K","N","Q","H","W","C","Y","G","E","D")
 apolar.aa<- c("T", "M", "I", "P", "L","V","F","A")
-
 v <- rep(NA, times=nrow(codon_table))
 v[!is.na(match(codon_table$aa, polar.aa))] <- "P"
 v[!is.na(match(codon_table$aa, apolar.aa))] <- "AP"
