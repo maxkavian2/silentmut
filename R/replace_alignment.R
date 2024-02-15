f5f6d26703870253f02a6053782dd550e57a1cae
Máximo Sánchez-Aragón <maxsanara@gmail.com>
2024-02-15 18:12:21 +0100

    Maxe's implements on 15th Feb.

--- R/replace_alignment.R
+++ R/replace_alignment.R
@@ -4,6 +4,7 @@
 #'@param codon.length The length of a codon, typically 3
 #'@param gcode Genetic code used. If \code{NULL}, the \code{GENETIC_CODE} object from the BiocManager package is used.
 #'@param gap.opening Indel penalty for ClustalW. Keeping this parameter high is recommended to ensure global aligments with no indels.
+#'@param ... Other arguments passed to \code{change.all.codons}.
 #'@return Returns a genetic sequence with silent mutations at the alignment region.
 #'@author Máximo Sánchez-Aragón.
 #'@example examples/silent_codon_aligned.R
@@ -20,7 +21,7 @@ replace.alignment <- function(sqsa, gcode = NULL, codon.length = 3, gap.opening 
   algpos <- c()
 
   #if(fls.flag){
-    seqsal <- multi.alignment(c(cdna.file, rnai.file), gapOpening=gap.opening, ...)
+    seqsal <- multi.alignment(c(cdna.file, rnai.file), gapOpening=gap.opening)
     algpos <- make.break.list(seqsal)[[2]]
     algpos[1] <- algpos[1] +1
 
@@ -55,8 +56,7 @@ replace.alignment <- function(sqsa, gcode = NULL, codon.length = 3, gap.opening 
   codpos <- codpos[restricted.codons[1]:restricted.codons[2]]
 
   sq <- change.all.codons(codpos, sq, gcode=codon_table,
-                          codon.length=codon.length, degenerate=FALSE,aa.col="aa",
-                          codon.col="sq")
+                          codon.length=codon.length, ...)
   sq
 }
 
